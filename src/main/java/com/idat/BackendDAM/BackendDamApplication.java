b0839c482d76fa413f27246b7565c3bdef518a6a
JesusPlasencia <jesusplasencia2508@hotmail.com>
2022-12-08 10:37:04 -0500

    C1 - Proyect Init

---
+++ src/main/java/com/idat/BackendDAM/BackendDamApplication.java
@@ -0,0 +1,13 @@
+package com.idat.BackendDAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/BackendDAM/BackendDamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.BackendDAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
